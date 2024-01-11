a0f3dfc042a1a87deb4e75523f06687c6251cb5c
Zumh <17211423+Zumh@users.noreply.github.com>
2024-01-11 10:39:21 -0500

    mongo db and front-end is not working for adding phone number

--- index.js
+++ index.js
@@ -75,32 +75,41 @@ app.get('/api/persons', (request, response) => {
  // how many entries are in the phonebook at the time of processing the request
 
 app.get('/info', (request, response) => {
-    
+  Person.find({}).then(persons => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
-    
+  })
+ 
 })
 
 
 // find the person by id from request
 // if found return person else return 404
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const personFound = persons.find(person => person.id === id)
+    // const id = Number(request.params.id)
+    // const personFound = persons.find(person => person.id === id)
     
-    if (personFound) {
-      response.json(personFound)
-    } else {
-      response.status(404).end()
-    }
+    // if (personFound) {
+    //   response.json(personFound)
+    // } else {
+    //   response.status(404).end()
+    // }
+
+    Person.findById(request.params.id).then(person => {
+      response.json(person)
+    })
 })
+
 
 // delete the person by id
 // if manage to delete return 204 else return 404
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+    // const id = Number(request.params.id)
+    // persons = persons.filter(person => person.id !== id)
+    // response.status(204).end()
+
+    Person.findByIdAndDelete(request.params.id).then(person => {
+      response.status(204).end()
+    })
 })
 
 
@@ -119,27 +128,42 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({ 
       error: 'name or number is missing' 
     })
-  } else if (persons.find(person => person.name === body.name)) {
+  }  
+  
+  // check if any duplicate name exist in the list before adding new one
+  Person.find({name: body.name}).then(person => {
+    if (person.length > 0) {
+      return response.status(400).json({ 
+        error: 'name must be unique' 
+      })
+    }
+  })
+  /*
+  if (persons.find(person => person.name === body.name)) {
     return response.status(400).json({ 
       error: 'name must be unique' 
     })
-  }
+  }*/
 
  
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
     id: generateId(),
-  }
+  })
 
-  // here we create a new persons array and send back to client
-  persons = persons.concat(person)
+  
+  //persons = persons.concat(person)
 
-  response.json(person)
+  // we are saving it in database and response after mongodb saved requested new data
+  // and the express server response with the new person that just added in database
+  person.save().then(savePerson => {
+    response.json(savePerson)
+  })
 })
 
 const PORT = process.env.PORT
-//const PORT = process.env.PORT || 3001
+//const PORT = process.env.PORT || 3001 // we need this for only when dotenv is not install in npm
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
